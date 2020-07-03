2d5e5818e8e125e1d320f4d12b7884ab5efc3f26
Ademar Alves de Oliveira <ademar111190@gmail.com>
2020-07-03 00:42:17 -0300

    Inital json reading

--- src/ui/by_year.rs
+++ src/ui/by_year.rs
@@ -1,10 +1,15 @@
 extern crate gio;
 extern crate gtk;
 
+use std::fs::File;
+use std::io::Read;
+use std::thread;
+
 use gtk::Box;
 
 use crate::data::strings::get_string;
 use crate::data::strings::StringId::{HomeLoading, StackYear};
+use crate::data::team::build_teams_payload;
 use crate::widget::center_box::CenterBox;
 use crate::widget::loading::Loading;
 
@@ -17,9 +22,23 @@ pub struct ByYear {
 pub fn build_by_year() -> ByYear {
     let loading = Loading::new(get_string(HomeLoading));
     let root = CenterBox::new(loading.widget);
+
+    thread::spawn(|| {
+        read_json();
+    });
+
     return ByYear {
         widget: root.widget,
         tag: String::from("StackYear"),
         title: get_string(StackYear),
     };
+}
+
+fn read_json() {
+    let mut file = File::open("assets/byTeam.json")
+        .expect("Failed to read the byTeam.json file");
+    let mut json = String::new();
+    file.read_to_string(&mut json);
+    let teams_payload = build_teams_payload(&json);
+    println!("> {:?}", teams_payload);
 }
